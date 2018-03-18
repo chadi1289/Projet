dc0bc628440d16a239d445dc0d0934e89dccf3c0
chadi1289 <chadiayachi@gmail.com>
2018-03-18 17:28:06 +0100

    Create bloc.java

---
+++ bloc.java
@@ -0,0 +1,18 @@
+/*https://medium.com/programmers-blockchain/create-simple-blockchain-java-tutorial-from-scratch-6eeed3cb03fa*/
+/*https://www.toutsurlebitcoin.fr/creez_votre_propre_blockchain_en_java.htm*/
+
+import java.util.Date;
+
+public class Block {
+	public String hash;
+	public String previousHash;
+	private String data; //our data will be a simple message.
+	private long timeStamp; //as number of milliseconds since 1/1/1970.
+
+	//Block Constructor.
+	public Block(String data,String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+	}
+}
