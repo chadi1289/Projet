d666ef3a2ddba628bd009ba5d4fe536aac87fa51
gpadid <37510476+gpadid@users.noreply.github.com>
2018-03-21 09:05:18 +0100

    Create Blockchain.java

---
+++ Blockchain.java
@@ -0,0 +1,21 @@
+/*ceci est une blockchain de test */
+
+import java.util.ArrayList;
+import Block.java;
+import com.google.gson.GsonBuilder;
+
+public class Blockchain {
+	
+	public static ArrayList<Block> blockchain = new ArrayList<Block>(); 
+
+	public static void main(String[] args) {	
+		//On ajoute des blocks à notre blockchain 
+		blockchain.add(new Block("Je suis le premier Block, "0"));		
+		blockchain.add(new Block("Je suis le second block",blockchain.get(blockchain.size()-1).hash)); 
+		blockchain.add(new Block("Et moi c'est le troisième block",blockchain.get(blockchain.size()-1).hash));
+		
+		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
+		System.out.println(blockchainJson);
+	}
+
+}
