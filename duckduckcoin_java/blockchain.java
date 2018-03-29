c3a767d842fb5a9b403a8ed4b41080f09873a83f
gpadid <37510476+gpadid@users.noreply.github.com>
2018-03-29 23:19:50 +0200

    Update blockchain.java

--- duckduckcoin_java/blockchain.java
+++ duckduckcoin_java/blockchain.java
@@ -1,20 +1,18 @@
-/*ceci est une blockchain de test */
-
+package Duckchain ;
 import java.util.ArrayList;
-import Block.java;
 import com.google.gson.GsonBuilder;
 
-public class Blockchain {
+public class Duckchain {
 
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
 	public static void main(String[] args) {
 		//On ajoute des blocks à notre blockchain
-		blockchain.add(new Block("Je suis le premier Block, "0"));
+		blockchain.add(new Block("Je suis le premier Block" , "0"));
 		blockchain.add(new Block("Je suis le second block",blockchain.get(blockchain.size()-1).hash));
 		blockchain.add(new Block("Et moi c'est le troisième block",blockchain.get(blockchain.size()-1).hash));
-
 		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
 		System.out.println(blockchainJson);
 	}
 }
+
