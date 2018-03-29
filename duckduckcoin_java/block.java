db6e0308c488f6ecf5d0b100d44e4cd4a594fd14
gpadid <37510476+gpadid@users.noreply.github.com>
2018-03-29 23:20:56 +0200

    Update block.java

--- duckduckcoin_java/block.java
+++ duckduckcoin_java/block.java
@@ -1,25 +1,33 @@
-import java.util.Date;
+package Duckchain;
+import java.util.*;
 public class Block {
-	/*Ici on va construire un block avec des attributs qui lui sont spécifiques ,
+	/*Ici on va créer un block avec des attributs qui lui sont spécifiques , 
 	utiliser cette classe pour créer un blockchain en rajoutant le merkle root */
 
 	public String hash;
-	public String previousHash;
-	private String data; //notre data sera un simple message
+	private String previousHash;
+	private String data; //notre data sera un simple message 
 	private long timeStamp;
-	private int nonce ;
 
 	//Constructeur de block
 	public Block(String data,String previousHash ) {
 		this.data = data;
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
-		this.hash = calculateHash();// on caclule le hash du code
+		this.setHash(calculateHash());// on caclule le hash du code 
 	}
 
 	public String calculateHash() {
-	String calculatedhash = StringUtil.applySha256(previousHash+Long(timeStamp)+data); //on utilise le  SHA256 fourni avec le projet
+	String calculatedhash = HashUtil.applySha256(previousHash+timeStamp+data); //on utilise le  SHA256 fourni avec le projet 
 	return calculatedhash;
 }
+
+	public String getHash() {
+		return hash;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
 
 }
