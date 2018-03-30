f2a3ff3840520741cc8ff58183da304538a17a2f
gpadid <37510476+gpadid@users.noreply.github.com>
2018-03-30 10:27:11 +0200

    Update blockchain.java

--- duckduckcoin_java/blockchain.java
+++ duckduckcoin_java/blockchain.java
@@ -1,24 +1,25 @@
 package Duckchain ;
-import java.util.ArrayList;
+import java.util.LinkedList;
+
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 
 public class Duckchain {
 
-	public static ArrayList<Block> blockchain = new ArrayList<Block>();
-
+	public static LinkedList<Block> blockchain = new LinkedList<Block>();
+	
 	public static void main(String[] args) {
 		//On ajoute des blocks à notre blockchain
-		blockchain.add(new Block("Je suis le premier Block" , "0"));
-		blockchain.add(new Block("Je suis le second block",blockchain.get(blockchain.size()-1).hash));
-		blockchain.add(new Block("Et moi c'est le troisième block",blockchain.get(blockchain.size()-1).hash));
-		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
-		System.out.println(blockchainJson);
+		blockchain.addLast(new Block("Je suis le premier Block" , "0"));
+		blockchain.addLast(new Block("Je suis le second block",blockchain.get(blockchain.size()-1).hash));
+		blockchain.addLast(new Block("Et moi c'est le troisième block",blockchain.get(blockchain.size()-1).hash));
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().registerTypeAdapter(Block.class, new BlockAdapter()).create();
+		JsonObject blockchainJson = new JsonObject();
+		
+		blockchainJson.add("blocks", gson.toJsonTree(blockchain));
+		
+		System.out.println(gson.toJson(blockchainJson));
 	}
-	
-	/*  public interface  Liste(Block){
-        void enfiler(Block b);
-        boolean estVide();
-        int longueur();
-    }*/
 }
-
